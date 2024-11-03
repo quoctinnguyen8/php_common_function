0a2f0fff034871ee9d881b493552863255c19fe0
quoctinnguyen8 <nguyenquoctin9x@gmail.com>
2024-11-03 18:11:30 +0700

    optimize source code

--- include/config.php
+++ include/config.php
@@ -4,14 +4,14 @@
  * Thay "php_common_function" bằng tên thư mục của bạn ở htdocs
  */
 
-// Thư mục gốc ở htdocs (đối với XAMPP)
+// Thư mục gốc ở htdocs (đối với XAMPP), để trống nếu không dùng htdocs của XAMPP
 define('ROOT_PATH', "/php_common_function");
 
 // Thư mục chứa file asset (css/js/img)
-define('ASSET_PATH', "/php_common_function/asset");
+define('ASSET_PATH', ROOT_PATH . "/asset");
 
 // Thư mục chứa file upload bởi user
-define('UPLOAD_PATH', "/php_common_function/upload");
+define('UPLOAD_PATH', ROOT_PATH . "/upload");
 
 // Đường dẫn đầy đủ đến thư mục hiện tại, không cần chỉnh sửa nếu dùng XAMPP
 define('DOCUMENT_ROOT_PATH', $_SERVER["DOCUMENT_ROOT"]);
